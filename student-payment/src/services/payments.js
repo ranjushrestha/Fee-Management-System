be6a1bb51b9b08973743c311517d099b59732033
Ranju Shrestha <ranjustha8844@gmail.com>
2025-11-15 20:45:23 +0545

    Create receipt

---
+++ student-payment/src/services/payments.js
@@ -0,0 +1,32 @@
+import { apiClient } from "./api";
+
+export async function listPayments() {
+    const {data} = await apiClient.get('/payments?_expand=student')
+    return data
+}
+
+export async function  createPayment(params) {
+    const {data} = await apiClient.post('/payments ', payment)
+    return data
+}
+
+export async function deletePayment(id) {
+    await apiClient.delete(`/payments ${id}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
